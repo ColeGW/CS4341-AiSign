b16443935549946a3dd374352a78aaf454dbf3c0
ColeGW <68122848+ColeGW@users.noreply.github.com>
2024-09-23 08:26:19 -0400

    jupyter notebook done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author='Cole Welcher',
+    author_email='cgwelcher@wpi.edu',
+    packages=find_packages(),
+    install_requires=[]
+)
